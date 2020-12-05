e45ace105fbcc3e8f5e10d89c69d5a42906b5e6a
Rusharound <33993101+Rusharound@users.noreply.github.com>
2020-12-05 09:26:32 -0500

    Sender code finished; need to confirm whether pins are double counting

--- main.ts
+++ main.ts
@@ -1,3 +1,47 @@
-basic.forever(function () {
-	
+input.onPinPressed(TouchPin.P0, function () {
+    Status_Code += 0.5
+    knight_rider_led()
+    Xmit_Latest(Status_Code)
 })
+input.onPinReleased(TouchPin.P0, function () {
+    Status_Code += 0.5
+    knight_rider_led()
+    Xmit_Latest(Status_Code)
+})
+function Xmit_Latest (num: number) {
+    for (let index = 0; index < Send_cycles; index++) {
+        radio.sendNumber(num)
+        basic.pause(200)
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    if (Sender == 1) {
+        Status_Code += 1
+        Xmit_Latest(Status_Code)
+    }
+})
+function knight_rider_led () {
+    for (let index = 0; index <= 4; index++) {
+        led.plot(index, 0)
+        basic.pause(100)
+    }
+    basic.clearScreen()
+}
+input.onButtonPressed(Button.B, function () {
+    if (Sender == 1) {
+        basic.showNumber(Status_Code)
+    }
+})
+let Status_Code = 0
+let Door_status = 0
+let Sender = 0
+let Send_cycles = 0
+radio.setGroup(12)
+Send_cycles = 5
+// earth's magnetic field is about 50 ut.   If a magnet is present, then device switches to sender mode
+if (input.magneticForce(Dimension.X) > 100) {
+    Sender = 1
+    Door_status = 1
+    Xmit_Latest(Status_Code)
+    knight_rider_led()
+}
