c6aa5c3366a37c28248f4a52dc49a4ece7096cc2
Rusharound <33993101+Rusharound@users.noreply.github.com>
2020-12-05 13:12:04 -0500

    Fixed magnet settings - but Pin 0 is pushed is not right.

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 input.onPinPressed(TouchPin.P0, function () {
-    Status_Code += 0.5
-    knight_rider_led()
-    Xmit_Latest(Status_Code)
+    if (Sender == 1) {
+        Status_Code += 1
+        Xmit_Latest(Status_Code)
+        basic.showNumber(Status_Code)
+        basic.pause(1000)
+    }
 })
 radio.onReceivedNumber(function (receivedNumber) {
     if (Sender == 0) {
@@ -28,9 +31,12 @@ radio.onReceivedNumber(function (receivedNumber) {
     }
 })
 input.onPinReleased(TouchPin.P0, function () {
-    Status_Code += 0.5
-    knight_rider_led()
-    Xmit_Latest(Status_Code)
+    if (Sender == 1) {
+        Status_Code += 1
+        Xmit_Latest(Status_Code)
+    }
+    basic.showNumber(Status_Code)
+    basic.pause(1000)
 })
 function Xmit_Latest (num: number) {
     for (let index = 0; index < Send_cycles; index++) {
@@ -52,6 +58,11 @@ function knight_rider_led () {
     }
     basic.clearScreen()
 }
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 4; index++) {
+        knight_rider_led()
+    }
+})
 input.onButtonPressed(Button.B, function () {
     if (Sender == 1) {
         basic.showNumber(Status_Code)
@@ -61,15 +72,19 @@ input.onButtonPressed(Button.B, function () {
 })
 let Local_Status_Code = 0
 let Status_Code = 0
-let Door_status = 0
 let Sender = 0
 let Send_cycles = 0
 radio.setGroup(12)
 Send_cycles = 5
 // earth's magnetic field is about 50 ut.   If a magnet is present, then device switches to sender mode
-if (input.magneticForce(Dimension.X) > 100) {
+if (Math.abs(input.magneticForce(Dimension.Strength)) > 350) {
     Sender = 1
-    Door_status = 1
     Xmit_Latest(Status_Code)
-    knight_rider_led()
+    for (let index = 0; index < 3; index++) {
+        knight_rider_led()
+    }
+} else {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(100)
+    basic.clearScreen()
 }
